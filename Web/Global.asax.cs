4449c35134c8e8ce7ebd9d9ca7f4064b32614386
ytodorov <ytodorov@ytodorov.com>
2016-10-17 20:59:50 +0300

    remove BundleConfig

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -14,8 +14,7 @@ namespace Web
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);           
         }
     }
 }
